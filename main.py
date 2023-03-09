e1f73e75ddd3185db4db07da2bcbabb30fd561a5
viktoriatsvkh <cvihviktoria2004@icloud.com>
2023-03-09 10:44:32 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+'''
+"Circus"
+Group:
+Tsvykh Viktoria
+Fishchukova Sofia
+'''
+
+def triangle(x, y, a, b, angle, color):
+    '''
+    Function for drawing triangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :return:
+    '''
+
+    pass
+
+def ellipse(x, y, a, b, color):
+    '''
+    Function for draw ellipse.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param color:
+    :return:
+    '''
+
+    pass
+
+def rectangle(x, y, a, b, color):
+    '''
+    Function for draw rectangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param color:
+    :return:
+    '''
+
+    pass
+
+
+def draw_elephant():
+    '''Drawing funny elephant on big ball'''
+
+    pass
+
+def draw_clown():
+    '''Drawing clown with balls'''
+
+    pass
+
+def main():
+    '''
+    Main function.
+    :return:
+    '''
+    draw_elephant()
+    draw_clown()
+
+
+if __name__ == '__main__':
+    main()
+    
